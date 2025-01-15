8850f1f46524e44e1e255861edfe0c6704cb0a4a
Christian Gabriel Lara Lopez <christianlaracs@gmail.com>
2025-01-15 18:00:09 -0500

    back to online mode

--- utils/qvapay_api.py
+++ utils/qvapay_api.py
@@ -7,12 +7,10 @@ from common.var import COINS
 class QvaPay:
     def __init__(self):
         # data for scraping
-        #self.base_url = "https://qvapay.com/api/p2p?page={number}"
-        #self.response = requests.get(self.base_url.format(number=1))
-        #self.data = json.loads(self.response.text)
-        #self.num_pages = self.data["last_page"]
-        
-        self.num_pages = 28
+        self.base_url = "https://qvapay.com/api/p2p?page={number}"
+        self.response = requests.get(self.base_url.format(number=1))
+        self.data = json.loads(self.response.text)
+        self.num_pages = self.data["last_page"]
         
         # simulating db
         # table order
@@ -37,13 +35,8 @@ class QvaPay:
         -------
         `void`
         """
-        # data = requests.get(self.base_url.format(number=page))
-        # data_json = json.loads(data.text)
-        
-        path = "qvapay/{number}.json"
-        json_file = open(path.format(number=page+1), "r")
-        json_read = json_file.read()
-        data_json = json.loads(json_read)
+        data = requests.get(self.base_url.format(number=page))
+        data_json = json.loads(data.text)
         
         for i in range(len(data_json["data"])):
             uuid = data_json["data"][i]["uuid"]
