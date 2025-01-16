18110e75f497718fb8b31068f508ea8ec0a53f23
Christian Gabriel Lara Lopez <christianlaracs@gmail.com>
2025-01-16 12:15:30 -0500

    app.py: fixed top parametrization in general view

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if update_btn:
 if symbol == "GENERAL":
     st.subheader("General")
     top = st.number_input("Top Market Makers", value=10, step=1)
-    market_maker = st.session_state.qva_pay.get_users_info_by_id(st.session_state.qva_pay.get_market_makers(100, "all"))
+    market_maker = st.session_state.qva_pay.get_users_info_by_id(st.session_state.qva_pay.get_market_makers(top, "all"))
     for index, user in enumerate(reversed(market_maker)):
         st.write(f"{index+1} - Nombre: {user["name"]} {user["lastname"]}")
         st.write(f"Total: ${st.session_state.qva_pay.user_coin_query[user["uuid"]]["all"]}")
